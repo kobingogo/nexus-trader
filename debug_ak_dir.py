8ea1fc57e817ed107d9a622c343c7d0e037138bd
Bingo <jianglingbin@1545939265451298.onaliyun.com>
2026-02-13 22:26:45 +0800

    chore: cleanup redundant scripts and untrack ignored files

--- debug_ak_dir.py
+++
@@ -1,3 +0,0 @@
-
-import akshare as ak
-print([m for m in dir(ak) if "concept" in m and "cons" in m])
--- debug_spot.py
+++
@@ -1,3 +0,0 @@
-
-import akshare as ak
-print([m for m in dir(ak) if "spot" in m and "stock" in m])
